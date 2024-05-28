a470adc5908efaf41611c7679f8ad18162533658
Giovani Bontempo <tempogiobon@gmail.com>
2024-05-28 18:48:52 -0300

    idles 2

--- scripts/main.js
+++ scripts/main.js
@@ -60,7 +60,7 @@ window.onYouTubeIframeAPIReady = function() {
     player = new YT.Player('player', {
         height: '600',
         width: '1280',
-        videoId: '_HQrM5L9T1g', // ID do vídeo inicial (pode ser vazio)
+        videoId: '7rdSlshLV9U&t', // ID do vídeo inicial (pode ser vazio)
         events: {
             'onReady': onPlayerReady,
             'onStateChange': onPlayerStateChange,
